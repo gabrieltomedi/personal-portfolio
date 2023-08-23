9f6f7fd047f832e7c9c866db082083ffdf18ed7e
Gabriel Oliveira Tomedi <gabriel.tomedi1@gmail.com>
2023-08-23 15:47:04 -0300

    added animations

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -43,7 +43,7 @@ export const Projects = () => {
         <section className="project" id="projects">
             <Container>
                 <Row>
-                    <Col>
+                    <Col size={12}>
                         <h2>Projects</h2>
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                         <Tab.Container id="projects-tabs" defaultActiveKey="first">
